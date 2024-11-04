b800331ab035f73d88fb6e64bf1b8d5d0090d3af
Azizkhodja309 <azizxojaazimxojayev81@gmail.com>
2024-11-04 15:47:20 +0500

    update method has added

--- src/toDoProject/ui/AndroidUI.java
+++ src/toDoProject/ui/AndroidUI.java
@@ -30,7 +30,7 @@ public class AndroidUI {
                     // deleteTask();
                     }
                 case 4->{
-                    // updateTask();
+                    updateTask();
                     }
                 case 0->{
                     break taskLoop;
@@ -39,5 +39,60 @@ public class AndroidUI {
                     System.out.println("Wrong menu! Try again!!!");
             }
         }
+    }
+
+    private static void updateTask() {
+        while (true) {
+            System.out.println("===== Update Task =====");
+            int taskLength = showTask();
+            System.out.println("0. Back");
+            int choose = Utils.getInt("Choose Menu: ");
+            if(choose == 0){
+                return;
+            }else if(choose < 0 || choose >= taskLength){
+                System.out.println("Wrong Menu! Try again!");
+                continue;
+            }
+            Task task = taskService.getTask(choose);
+
+            System.out.println("""
+                    ===== Update Menu =====
+                    1. Name
+                    2. Description
+                    3. Task Status
+                    0. Back
+                    """);
+            int anInt = Utils.getInt("Choose Menu: ");
+
+            switch (anInt) {
+                case 1:
+                    String newName = Utils.getStr("Enter Tasks New Name: ");
+                    task.setName(newName);
+                    break;
+                case 2:
+                    String newDescription = Utils.getStr("Enter Tasks New Description: ");
+                    task.setDescription(newDescription);
+                    break;
+                case 3:
+                    System.out.println(TaskStatus.getAllValuesAsString());
+                    Integer statusOrder = Utils.getInt("Choose status: ");
+                    TaskStatus newStatus = TaskStatus.getValueByOrder(statusOrder);
+                    task.setStatus(newStatus);
+                    break;
+                default:
+                    System.out.println("Something wrong! Try again !!!");
+            }
+        }
+    }
+
+    private static int showTask(){
+        System.out.println("===== Task Menu =====");
+        Task[] all = taskService.getAllTasks();
+        int count = 1;
+        for (Task task : all) {
+            System.out.println(count + ". " + task.getName());
+            count++;
+        }
+        return count;
     }
 }
